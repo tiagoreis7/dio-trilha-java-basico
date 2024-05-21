d74b4d47223fa79342b6c1f726f29cd9dedee1a7
tiagoreis7 <tiagoreisaraujo7@gmail.com>
2024-05-21 11:44:44 -0300

    update

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 5;
+
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+    }
+}
