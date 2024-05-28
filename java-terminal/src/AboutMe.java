155674d25b62facf9c3c2cff366e3700cd73c99e
tiagoreis7 <tiagoreisaraujo7@gmail.com>
2024-05-28 19:47:53 -0300

    update

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,11 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        //os argumentos começam co indice 0
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
-        double altura = Double.valueOf(args [3]);
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+        
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
 
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("================================");
+        //imprimindo os dados obtidos pelo usuario
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
