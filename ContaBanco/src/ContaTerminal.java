ce455b6d3383fc02b64a470ce43851f1a33c7c57
tiagoreis7 <tiagoreisaraujo7@gmail.com>
2024-05-28 23:10:06 -0300

    codigo inserido para cadastro de conta no banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double saldo = 750.50f;
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+        
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Informe o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco," + 
+        " sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+
     }
 }
