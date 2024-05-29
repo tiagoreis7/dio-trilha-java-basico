5832ae43c6c2e985566611d86b6e2c7f196d1a75
Tiago Reis <41928844+tiagoreis7@users.noreply.github.com>
2024-05-28 23:29:04 -0300

    Atualizar o ContaTerminal.java
    Comentário sobre a utilização do printf

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -17,7 +17,7 @@ public class ContaTerminal {
 
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco," + 
         " sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+        //aqui usei um print formatado "printf"
 
     }
 }
-//
